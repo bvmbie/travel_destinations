654f24cb1b52504b7e619ed6b4e98f380e3d49e7
Henna Vahatiitto <hnnamv@gmail.com>
2020-02-10 10:36:08 +0000

    first commit

---
+++ lib/travel_destinations.rb
@@ -0,0 +1,6 @@
+require "travel_destinations/version"
+
+module TravelDestinations
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/travel_destinations/version.rb
@@ -0,0 +1,3 @@
+module TravelDestinations
+  VERSION = "0.1.0"
+end
