952693c5da8328986a539d1bb810f2d7917e32b5
Henna Vahatiitto <hnnamv@gmail.com>
2020-02-10 17:29:26 +0000

    changes

--- lib/travel_destinations/destination.rb
+++ lib/travel_destinations/destination.rb
@@ -11,18 +11,16 @@ class TravelDestinations::Destination
     @@all << self.scrape
   end
 
-   def self.scrape
+  def self.titles
     doc = Nokogiri::HTML(open("https://www.cntraveller.com/gallery/best-holiday-destinations-2020"))
-    binding.pry
-    # continue 53 here
-    # titles = doc.search("div.c-figure__title").text.split(/\d|\./).reject(&:empty?).reverse
-    # => [" Matera, Italy", " Valle de Guadalupe", " New Orleans", " TASMANIA, AUSTRALIA", " Transylvania, Romania", " Egypt", " Chengdu, China", " Arles, France", " Patagonia", " Namibia", " Tahiti", " The Peloponnese, Greece", " Mumbai", " Mozambique", " St Barth’s, Caribbean", " THE SCOTTISH HIGHLANDS", " SINGAPORE", " PERTH, WESTERN AUSTRALIA", " THE TURKISH RIVIERA", " Pakistan", " Plymouth, UK", " Kyrgyzstan", " Armenia", " Salvador, Brazil", " Kangaroo Island, Australia", " Galway, Ireland", " Siargao, Philippines", " Paris, France", " Egadi Islands, Sicily", " Dakar, Senegal", " Portland, Maine, US", " Lebanon", " Qingdao, China", " Frisian Islands, Denmark", " The British Virgin Islands", " Rabat, Morocco", " Panama", " Rijeka, Croatia", " Kyoto, Japan"]
-    #titles.each.with_index(1) do |value, index|
-    #puts "#{index} #{value}"
-    #end
     
+    titles = doc.search("div.c-figure__title").text.split(/\d|\./).reject(&:empty?).reverse
+    # titles_list = titles.each.with_index(1) do |value, index|
+    #  puts "#{index} #{value}"
+    # end
+    # titles_list
   end
   
-
+    # continue 54 here
 
 end
