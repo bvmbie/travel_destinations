fe7ff203ee624d9f44770dc2fac6d095dae5bf15
Henna Vahatiitto <hnnamv@gmail.com>
2020-02-10 16:20:21 +0000

    changes

--- lib/travel_destinations/destination.rb
+++ lib/travel_destinations/destination.rb
@@ -17,6 +17,7 @@ class TravelDestinations::Destination
     # continue 53min here
     # title = doc.search("div.c-figure__title").text 
     # => "20. Kyoto, Japan19. Rijeka, Croatia18. Panama17. Rabat, Morocco16. The British Virgin Islands15. Frisian Islands, Denmark14. Qingdao, China13. Lebanon12. Portland, Maine, US11. Dakar, Senegal10. Egadi Islands, Sicily9. Paris, France8. Siargao, Philippines7. Galway, Ireland6. Kangaroo Island, Australia5. Salvador, Brazil4. Armenia3. Kyrgyzstan2. Plymouth, UK1. Pakistan19. THE TURKISH RIVIERA18. PERTH, WESTERN AUSTRALIA17. SINGAPORE16. THE SCOTTISH HIGHLANDS15. St Barth’s,Caribbean14. Mozambique13. Mumbai12. The Peloponnese, Greece11. Tahiti10. Namibia9. Patagonia8. Arles, France7. Chengdu, China6. Egypt5. Transylvania, Romania4. TASMANIA, AUSTRALIA3. New Orleans2. Valle de Guadalupe1. Matera, Italy"
+    # kyoto = p doc.css("div.c-figure__title").text[0..19]
   end
 
 end
